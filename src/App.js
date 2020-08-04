45db14d36b9429fbe6b82aa893e97ae75d2c277d
ailufarana <68763361+ailufarana@users.noreply.github.com>
2020-08-04 20:52:20 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { CardList } from "./components/card-list/cardlist.component";
+import { SearchBox } from "./components/searchbox/search-box.component";
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+   
+      this.state = {
+        monsters: [],
+        searchField: ''
+      }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}))
+  }
+
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(
+     monsters => monsters.name.toLowerCase().includes(searchField.toLowerCase())     
+    )
+
+
+
+
+    return(
+      <div className='App'>
+      
+        <SearchBox
+          placeholder = 'Search Monsters'
+          handleChange = {e => this.setState({ searchField: e.target.value})}
+        
+        />
+
+
+        <CardList monsters={filteredMonsters}>
+       
+        </CardList>
+      </div>
+    )
+  }
+}
+
+export default App;
